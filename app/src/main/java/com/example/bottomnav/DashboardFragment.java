ce600a2a8905b72a8aade288d66c185d2cdf85d5
leo <murieljack1@gmail.com>
2019-02-28 17:57:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bottomnav/DashboardFragment.java
@@ -0,0 +1,24 @@
+package com.example.bottomnav;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+    public DashboardFragment() {
+    }
+
+    public static DashboardFragment newInstance(){
+        return new DashboardFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard, container,false);
+    }
+}
